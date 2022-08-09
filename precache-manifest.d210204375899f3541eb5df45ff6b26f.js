e1fd1bea2bd2b6c1e42aaec40a74dc92929c8947
limdp99 <dlim@pm-studios.com>
2022-08-09 15:28:54 +0900

    Updates

---
+++ precache-manifest.d210204375899f3541eb5df45ff6b26f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2182ebeb09cd30131083",
+    "url": "/aki-pages/static/css/main.5a7776cc.chunk.css"
+  },
+  {
+    "revision": "2182ebeb09cd30131083",
+    "url": "/aki-pages/static/js/main.2182ebeb.chunk.js"
+  },
+  {
+    "revision": "b613cb6236948eeb1814",
+    "url": "/aki-pages/static/js/1.b613cb62.chunk.js"
+  },
+  {
+    "revision": "b589ddbd33c45c554718",
+    "url": "/aki-pages/static/js/runtime~main.b589ddbd.js"
+  },
+  {
+    "revision": "fcb6de9390a7eb2feb9bdbdd52262a53",
+    "url": "/aki-pages/index.html"
+  }
+];
